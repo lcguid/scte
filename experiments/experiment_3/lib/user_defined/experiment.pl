31b4b80235ac8b815a8b3ce803f7de4dd5af86da
Luiz C. Mostaco-Guidolin <lm.guidolin@gmail.com>
2011-10-28 17:14:18 -0500

    copyright notice updated

--- experiments/experiment_3/lib/user_defined/experiment.pl
+++ experiments/experiment_3/lib/user_defined/experiment.pl
@@ -1,20 +1,22 @@
 #!/usr/bin/perl -w          
-#  Copyright 2004-2011 Luiz Carlos Büttner Mostaço-Guidolin
-#  
-#  This file is part of SCTE.
+
+#______________________________________________________________________________ 
 #
-#  SCTE is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  SCTE is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with SCTE. If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2004-2011 Luiz C. Mostaço-Guidolin
+# 
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later 
+# version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#______________________________________________________________________________
+
 
 =pod
 * WaveFormCapture() *
--- experiments/experiment_3/lib/user_defined/legacy.pl
+++ experiments/experiment_3/lib/user_defined/legacy.pl
@@ -1,19 +1,20 @@
-#  Copyright 2004-2011 Luiz Carlos Büttner Mostaço-Guidolin
-#  
-#  This file is part of SCTE.
+#______________________________________________________________________________ 
 #
-#  SCTE is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  SCTE is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with SCTE. If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2004-2011 Luiz C. Mostaço-Guidolin
+# 
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later 
+# version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#______________________________________________________________________________
+
 
 use Term::ANSIColor qw(:constants);
   $Term::ANSIColor::AUTORESET = 1;
