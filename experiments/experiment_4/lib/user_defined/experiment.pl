76177c0b83e0940c9f0f72b302a4dae0090149b8
Luiz C. Mostaco-Guidolin <lm.guidolin@gmail.com>
2012-01-12 18:56:58 -0600

    Bug Fixes

--- experiments/experiment_4/lib/user_defined/experiment.pl
+++ experiments/experiment_4/lib/user_defined/experiment.pl
@@ -23,7 +23,8 @@ sub RunExperiment
   $dev->Write( "HARDCopy ABOrt" );
   
   # start capturing image data
-  ${$PSreadings} = $dev->SerialWriteBuffered("HARDCopy STARt\n") ;
+  # ${$PSreadings} = $dev->SerialWriteBuffered("HARDCopy STARt\n") ;
+  ${$PSreadings} = $dev->Write("HARDCopy STARt\n") ;
 }
 
 1;
